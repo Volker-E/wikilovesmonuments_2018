bb4d67ea0bed57d16c93c717b38039e61d8acb04
Volker E <volker.e@wikimedia.org>
2018-10-12 14:37:20 -0700

    Add post thumbnail images to post lists

--- content.php
+++ content.php
@@ -12,6 +12,17 @@
 <article <?php post_class( 'blog-entry' ); ?>>
 	<div class="entry">
 		<header>
+		<?php if ( has_post_thumbnail() ) {
+			$thumbnail = wp_get_attachment_image_src( get_post_thumbnail_id( $post->ID ), 'wikilovesmonuments-featured-image' );
+
+			// Calculate aspect ratio: h / w * 100%.
+			$ratio = $thumbnail[2] / $thumbnail[1] * 100;
+			?>
+
+			<div class="entry__thumb" style="background-image: url(<?php echo esc_url( $thumbnail[0] ); ?>);">
+				<div class="entry__thumb-prop" style="padding-top: <?php echo esc_attr( $ratio ); ?>%"></div>
+			</div>
+		<?php } ?>
 			<h2 class="entry__title"><a href="<?php the_permalink(); ?>" title="<?php the_title(); ?>"><?php the_title(); ?></a></h2>
 		</header>
 		<?php
