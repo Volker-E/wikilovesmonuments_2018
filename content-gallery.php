6f2068713a7f61f6ec519ad7d1df72ed5d973812
Volker E <volker.e@wikimedia.org>
2018-10-12 15:24:13 -0700

    Follow-up comments link cleanup
    Also use the same code in 'content-gallery' & 'single' templates.

--- content-gallery.php
+++ content-gallery.php
@@ -10,14 +10,14 @@
 ?>
 
 <article <?php post_class( 'blog-entry' ); ?>>
-	<?php
-    // Get resize and show featured image: refer to functions/img_defaults.php for default values
-    $wikilovesmonuments_entry_img = aq_resize( wp_get_attachment_url( get_post_thumbnail_id(), 'full' ), wikilovesmonuments_img( 'blog_entry_width' ), wikilovesmonuments_img( 'blog_entry_height' ), wikilovesmonuments_img( 'blog_entry_crop' ) );
-    	if ( $wikilovesmonuments_entry_img ) { ?>
-        <div class="blog-entry-thumbnail">
-            <a href="<?php the_permalink(); ?>" title="<?php the_title(); ?>"><img src="<?php echo $wikilovesmonuments_entry_img; ?>" alt="<?php echo the_title(); ?>"></a>
-        </div>
-    <?php } ?>
+<?php
+// Get resize and show featured image: refer to functions/img_defaults.php for default values
+$wikilovesmonuments_entry_img = aq_resize( wp_get_attachment_url( get_post_thumbnail_id(), 'full' ), wikilovesmonuments_img( 'blog_entry_width' ), wikilovesmonuments_img( 'blog_entry_height' ), wikilovesmonuments_img( 'blog_entry_crop' ) );
+	if ( $wikilovesmonuments_entry_img ) { ?>
+    <div class="blog-entry-thumbnail">
+        <a href="<?php the_permalink(); ?>" title="<?php the_title(); ?>"><img src="<?php echo $wikilovesmonuments_entry_img; ?>" alt="<?php echo the_title(); ?>"></a>
+    </div>
+<?php } ?>
     <div class="entry">
         <header>
             <h2><a href="<?php the_permalink(); ?>" title="<?php the_title(); ?>"><?php the_title(); ?></a></h2>
@@ -31,7 +31,9 @@
 		<ul class="entry__meta">
             <li><?php the_author_posts_link(); ?> <span class="is-aural">posted</span> on <?php echo get_the_date(); ?></li>
             <?php
-            if ( comments_open() ) { ?><li class="lnk--comments"><?php comments_popup_link( __( '0 Comments', 'wikilovesmonuments' ), __( '1 Comment', 'wikilovesmonuments' ), __( '% Comments', 'wikilovesmonuments' ), 'comments-link' ); ?></li><?php } ?>
+            if ( comments_open() ) { ?>
+            <li><?php comments_popup_link( __( '0 Comments', 'wikilovesmonuments' ), __( '1 Comment', 'wikilovesmonuments' ), __( '% Comments', 'wikilovesmonuments' ), 'lnk--comments' ); ?></li>
+            <?php } ?>
 		</ul>
     </div>
 </article>
--- single.php
+++ single.php
@@ -36,7 +36,10 @@ if ( have_posts() ) {
 				<h1><?php the_title(); ?></h1>
 				<ul class="entry__meta">
 					<li><?php the_author_posts_link(); ?> <span class="is-aural">posted</span> on <?php echo get_the_date(); ?></li>
-					<?php if ( comments_open() ) { ?><li class="lnk--comments"><?php comments_popup_link( __( '0 Comments', 'wikilovesmonuments' ), __( '1 Comment', 'wikilovesmonuments' ), __( '% Comments', 'wikilovesmonuments' ), 'comments-link' ); ?></li><?php } ?>
+		            <?php
+		            if ( comments_open() ) { ?>
+		            <li><?php comments_popup_link( __( '0 Comments', 'wikilovesmonuments' ), __( '1 Comment', 'wikilovesmonuments' ), __( '% Comments', 'wikilovesmonuments' ), 'lnk--comments' ); ?></li>
+		            <?php } ?>
 				</ul>
 			</header>
 		<?php } ?>
