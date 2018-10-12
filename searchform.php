d122ea6af712f507eac1a5a4798799e9aa9da2d8
Volker E <volker.e@wikimedia.org>
2018-10-12 15:29:22 -0700

    Improve searchform

--- searchform.php
+++ searchform.php
@@ -8,4 +8,6 @@
  * @version 0.7.0
  */
 ?>
-<form method="get" id="searchbar" action="<?php echo home_url( '/' ); ?>"><input type="search" name="s" value="<?php _e( 'Type and hit enter to search', 'wikilovesmonuments' ); ?>" onfocus="if(this.value==this.defaultValue)this.value='';" onblur="if(this.value=='')this.value=this.defaultValue;"></form>
+<form method="get" id="searchbar" action="<?php echo home_url( '/' ); ?>">
+	<input type="search" name="s" value="<?php _e( 'Type and hit enter to search', 'wikilovesmonuments' ); ?>" onfocus="if(this.value==this.defaultValue)this.value='';" onblur="if(this.value=='')this.value=this.defaultValue;">
+</form>
